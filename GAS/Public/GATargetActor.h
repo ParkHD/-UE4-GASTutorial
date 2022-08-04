55120bee3e5dd453fb2311d3fbefefacd2a9f660
ParkHD <76206050+ParkHD@users.noreply.github.com>
2022-08-04 17:42:22 +0900

    * GameAbilityTargetActor

---
+++ GAS/Public/GATargetActor.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "GATargetActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGATargetActor : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+public:
+
+	AGATargetActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn = true), Category= "GASGameplayAbility")
+		float TraceRange;
+
+	bool LineTraceFN(FHitResult& TraceHitResult);
+
+	virtual void StartTargeting(UGameplayAbility* Ability) override;
+	virtual void ConfirmTargetingAndContinue() override;
+};
